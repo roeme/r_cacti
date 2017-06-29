1fedbc2c92dbbf33a1cefe93807104710f7299a1
roeme <git@roeme.ch>
2017-06-29 15:31:11 +0200

    syntax yet again

--- manifests/webinstance/debian.pp
+++ manifests/webinstance/debian.pp
@@ -5,7 +5,7 @@ class r_cacti::webinstance::debian inherits r_cacti::webinstance {
   if $manage_packages {
     ensure_packages([
       'libapache2-mod-php7.0',
-      'php7.0'
+      'php7.0',
       'php7.0-mysql',
       'php7.0-mbstring'],{'ensure' => 'present'})
   }
