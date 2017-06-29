14704eb2218c2dba1de18b309108ed7f2e1bdf5e
roeme <git@roeme.ch>
2017-06-29 12:43:51 +0200

    dl/install source

---
+++ manifests/install_source.pp
@@ -0,0 +1,19 @@
+# Class: r_cacti::install_source inherits r_cacti
+#
+#
+class r_cacti::install_source inherits r_cacti {
+  include archive
+  archive { 'cacti-source':
+    ensure        => present,
+    extract       => true,
+    extract_path  => "${target_dir_base}",
+    source        => "${relsource}",
+    creates       => "${target_dir_base}/cacti-release-${version}",
+    cleanup       => true,
+  }
+  file { "${target_dir_base}/cacti":
+    ensure        => 'link',
+    target        => "${target_dir_base}/cacti-release-${version}",
+    require       => Archive['cacti-source'],
+  }
+}
