9a0012d83b82a85f22414af7ca7e6faa7f8a3f43
roeme <git@roeme.ch>
2017-06-29 13:36:04 +0200

    clean stuff

--- manifests/webinstance/debian.pp
+++ manifests/webinstance/debian.pp
@@ -2,10 +2,5 @@
 #
 #
 class r_cacti::webinstance::debian inherits r_cacti::webinstance::debian {
-  include apache
-  apache::vhost { 'cacti-vhost':
-    docroot       => "$target_dir_base/cacti",
-    docroot_owner => $cacti_user,
-    docroot_group => $cacti_group,
-  }
+  # not yet
 }
