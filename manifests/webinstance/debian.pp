f343a0bc2ad3b94a6e164f3b2de4a59e1a66a6cf
roeme <git@roeme.ch>
2017-06-29 14:03:12 +0200

    enable php7.0 mod under debian

--- manifests/webinstance/debian.pp
+++ manifests/webinstance/debian.pp
@@ -4,5 +4,6 @@
 class r_cacti::webinstance::debian inherits r_cacti::webinstance {
   if $manage_packages {
     ensure_packages(['libapache2-mod-php7.0','php7.0'],{'ensure' => 'present'})
+    apache::mod { 'php7.0': }
   }
 }
