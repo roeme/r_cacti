67443c2bbd1f480d20f131bea749adc2a9857799
roeme <git@roeme.ch>
2017-06-29 14:14:38 +0200

    tell apache::mod where to find the lib

--- manifests/webinstance/debian.pp
+++ manifests/webinstance/debian.pp
@@ -4,6 +4,8 @@
 class r_cacti::webinstance::debian inherits r_cacti::webinstance {
   if $manage_packages {
     ensure_packages(['libapache2-mod-php7.0','php7.0'],{'ensure' => 'present'})
-    apache::mod { 'php7': }
+    apache::mod { 'php7.0':
+      path => '/usr/lib/apache2/modules/libphp7.0.so',
+    }
   }
 }
