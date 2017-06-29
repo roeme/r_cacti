3ae5184f5ec7af2ceaf0a3eb5891632a04cfbcbb
roeme <git@roeme.ch>
2017-06-29 12:45:19 +0200

    parameter interpolation, plus source install

--- manifests/webinstance.pp
+++ manifests/webinstance.pp
@@ -5,12 +5,15 @@ class r_cacti::webinstance inherits r_cacti {
   if $mkvhost {
     include apache
     apache::vhost { 'cacti-vhost':
-      docroot       => "$target_dir_base/cacti",
+      docroot       => "${target_dir_base}/cacti",
       docroot_owner => $cacti_user,
       docroot_group => $cacti_group,
       serveraliases => $vhostdomain,
     }
   }
+
+  include r_cacti::install_source
+
   case $osfamily {
     'Debian': {
       include r_cacti::webinstance::debian
